75db311ee8734a5ad7b5aee6b3fa4a58360ce0d8
HelloHappySteve <121426383+HelloHappySteve@users.noreply.github.com>
2023-02-25 23:56:13 +0800

    Create mydescribe.py

---
+++ mydescribe.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import time
+container = st.container()
+time.sleep(0.2)
+container.markdown('# `Hello`')
+container.markdown('# `My name is Steve Brown`')
+container.markdown("# `I'm from QingHua Middle School`")
+container.markdown('# `This is my first website`')
+container.markdown('# `If you want to know about me`')
+container.markdown('# `-*----**----***--**---*-**---`')
+container.markdown('# `......`')

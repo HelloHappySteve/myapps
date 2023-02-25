ea8a6f584e1d2deb8aea5772707d09012b070ffa
HelloHappySteve <121426383+HelloHappySteve@users.noreply.github.com>
2023-02-26 01:14:53 +0800

    Update mydescribe.py

--- mydescribe.py
+++ mydescribe.py
@@ -1,11 +1,28 @@
-import streamlit as st
 import time
-container = st.container()
-time.sleep(0.2)
-container.markdown('# `Hello`')
-container.markdown('# `My name is Steve Brown`')
-container.markdown("# `I'm from QingHua Middle School`")
-container.markdown('# `This is my first website`')
-container.markdown('# `If you want to know about me`')
-container.markdown('# `-*----**----***--**---*-**---`')
-container.markdown('# `......`')
+import streamlit as st
+container = st.empty()
+for i in range(50):
+    with container:
+        st.text('''
+            ██████████  ██████████        ██        ████████    ██████████
+            ██      ██      ██          ██  ██      ██      ██      ██
+            ██              ██          ██  ██      ██      ██      ██
+            ██████████      ██        ██      ██    ████████        ██
+                    ██      ██        ██████████    ████            ██
+                    ██      ██        ██      ██    ██  ████        ██
+            ██████████      ██      ██          ██  ██      ██      ██
+        ''')
+    time.sleep(0.1)
+    with container:
+        st.text('''
+            ██████████  ██████████        ██        ████████    ██████████  
+            ██      ██      ██          ██  ██      ██      ██      ██      
+            ██              ██          ██  ██      ██      ██      ██      
+            ██████████      ██        ██      ██    ████████        ██      
+                    ██      ██        ██████████    ████            ██        ██
+                    ██      ██        ██      ██    ██  ████        ██      ██  ██
+            ██████████      ██      ██          ██  ██      ██      ██        ██
+        ''')
+    time.sleep(0.1)
+time.sleep(0.5)
+container.empty()

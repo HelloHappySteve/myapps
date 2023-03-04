532c3aa6bf3cc5f05a06ab744fe8480d291976f9
HelloHappySteve <121426383+HelloHappySteve@users.noreply.github.com>
2023-03-04 10:05:48 +0800

    Update one-all.py

--- one-all.py
+++ one-all.py
@@ -1,38 +1,37 @@
 import time
 import streamlit as st
 st.set_page_config(
-    page_title='One Is All',
+    page_title='ClDesk',
     page_icon='🌐',
     layout='wide',
     initial_sidebar_state='collapsed',
     menu_items={}
 )
-
-container = st.empty()
-for i in range(10):
-    with container:
-        st.text('''
-            Start
-        ''')
+with st.spinner(''):
+    container = st.empty()
+    for i in range(10):
+        with container:
+            st.text('''
+                Start
+            ''')
+        time.sleep(0.5)
+        with container:
+            st.text('''
+                Start .
+            ''')
+        time.sleep(0.5)
+        with container:
+            st.text('''
+                Start ..
+            ''')
+        time.sleep(0.5)
+        with container:
+            st.text('''
+                Start ...
+            ''')
+        time.sleep(0.5)
     time.sleep(0.5)
-    with container:
-        st.text('''
-            Start .
-        ''')
-    time.sleep(0.5)
-    with container:
-        st.text('''
-            Start ..
-        ''')
-    time.sleep(0.5)
-    with container:
-        st.text('''
-            Start ...
-        ''')
-    time.sleep(0.5)
-time.sleep(0.5)
-container.empty()
-
+    container.empty()
 with st.columns([1,1,1])[1]:
     st.text('''
         Index Page
